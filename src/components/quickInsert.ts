4faf4ebc877227141c3b409b7145e7853087a0b4
Simon Winter <simon.winter@stz-softwaretechnik.de>
2021-05-25 14:39:36 +0200

    Removed Bold since latex workshop already has it

--- src/components/quickInsert.ts
+++ src/components/quickInsert.ts
@@ -8,39 +8,6 @@ export class quickInsert {
     constructor(extension: Extension) {
         this.extension = extension
     }
-    public bold() {
-        if (!vscode.window.activeTextEditor) {
-            return
-        }
-        const editor = vscode.window.activeTextEditor
-        editor.edit(editBuilder => {
-            for (const selection of editor.selections) 
-            {
-
-                
-                let textOfNewSelection = editor.document.getText(selection)
-                if (editor.document.getText(selection).match(/\\textbf/)) {
-                    editBuilder.replace(new vscode.Range(selection.start, selection.end),
-                        textOfNewSelection.replace("\\textbf{", "")
-                            .replace(/(})(?!.*})/gs, "")
-                    )
-                }
-                else
-                {
-                    const selectedContent = editor.document.getText(selection)
-                    const selectedCommand = '\\textbf{' + selectedContent + "}"
-                    editBuilder.replace(new vscode.Range(selection.start, selection.end),
-                        selectedCommand.replace(/(.*)(\${\d.*?})/, `$1${selectedContent}`)
-                            .replace(/\${\d:?(.*?)}/g, '$1')
-                            .replace('\\\\', '\\')
-                            .replace(/\$\d/, ''))
-                }
-
-                
-            }
-        })
-
-    }
 
     public cleanFormating() {
         if (!vscode.window.activeTextEditor) {
